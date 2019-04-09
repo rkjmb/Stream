045ad675a067ecb895a8f4f62e969e95170dd9bc
rkjmb <rkjmb123@gmail.com>
2019-04-09 22:56:55 +0530

    Create new stream and view all

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends Component {
+  renderError = ({ error, touched }) => {
+    if (error && touched) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "Required";
+  } else if (formValues.title.length > 15) {
+    errors.title = "Must be 15 characters or less";
+  }
+  if (!formValues.description) {
+    errors.description = "Required";
+  }
+  return errors;
 };
 
-export default StreamCreate;
+const formWrapped = reduxForm({
+  form: "streamCreate",
+  validate
+})(StreamCreate);
+
+export default connect(
+  null,
+  { createStream }
+)(formWrapped);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import authReducer from "../reducers/authReducer";
+import streamReducer from "../reducers/streamReducer";
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer,
+  streams: streamReducer
 });
